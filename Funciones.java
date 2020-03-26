c61257a73041bf4345f8c8af8d295ceb3f717aa3
Josearemos <josearemos01@gmail.com>
2020-03-26 12:45:47 +0100

    F5 #7

--- Funciones.java
+++ Funciones.java
@@ -53,6 +53,23 @@ public class Funciones {
 
 		return CadConca;
 	}
+public class Funcion5{
+	public static void main(String[] args) {
+		int [] array = {22, 45, 25};
+		System.out.println(Funcion5(array));
+	}
+	public static int Funcion5(int [] no) {
+
+		int tamano = no.length;
+		int suma = 0;
+		int [] noarray = new int [tamano];
+
+		for (int con = 0; con < tamano; con++) {
+			noarray[con] = no[con];
+			suma = suma + noarray[con];
+		}
+		return suma;
+	}
 
 }
 
