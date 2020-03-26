9f07ef9214265464b3a4f3faae816093d0a72617
Josearemos <josearemos01@gmail.com>
2020-03-26 12:35:38 +0100

    F3 #4

--- Funciones.java
+++ Funciones.java
@@ -1,9 +1,9 @@
-public class Funcion2 {
+public class Funciones {
 	public boolean Funcion1(int no) {
 		return no % 23 == 0;
 	}
 
-	public Boolean Funcion2(int numeroPrueba) {
+	public boolean Funcion2(int numeroPrueba) {
 
 		Boolean prueb = null;
 		int Y = 700;
@@ -16,6 +16,24 @@ public class Funcion2 {
 		prueb = false;
 	}
 	return prueb;
+
 	}
+
+	public int Funcion3(int numero) {
+
+		int x = 23;
+		int y = 600;
+		int y1 = 700;
+		int r = (int) Math.floor(Math.random()*(y1+y));
+	    int result = 0;
+
+		if (numero%x == 0) {
+			result = numero%r;
+		}
+		return result;
+	 }
 }
-}
+
+
+
+
