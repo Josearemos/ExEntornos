bf7e220c0e7c19968bab0e6e2df2cd09e84f934b
Josearemos <josearemos01@gmail.com>
2020-03-26 12:17:36 +0100

    F1 #2

---
+++ Funciones.java
@@ -0,0 +1,8 @@
+public class Funcion1 {
+	public static void main(String[] args) {
+		System.out.println(Funcion1(Integer.MAX_VALUE));
+	}
+	public static boolean Funcion1(int no) {
+		return no % 23 == 0;
+	}
+}
